2440d10a908d111eae1aa2ae57cd9a155ffd0018
LAPTOP-B7QEB5QK\MAGNUM <dkwl7whd1004@gmail.com>
2025-01-16 21:40:04 +0900

    SOLID 원칙

--- GRIMPAN/src/grimpan.ts
+++ GRIMPAN/src/grimpan.ts
@@ -19,3 +19,111 @@ class Grimpan{
     }
 }
 export default Grimpan;
+
+//OCP: 개방폐쇄 원칙
+//위반
+function main(type){
+    if(type === 'a'){
+        doA();
+    }else if(type === 'b'){
+        doB();
+    }else if(type === 'c'){
+        doC();
+    }else if(type === 'd'){
+        doD();        
+    }else{
+        
+    }
+}
+
+//해결
+interface Doable {
+    do(): void;
+}
+function main(type: Doable){
+    type.do();
+}
+const a = {do(){}}
+const b = {do(){}}
+const c = {do(){}}
+
+
+//LSP: 리스코프치환 원칙
+class Animal{
+    isAnimal(){
+        return true;
+    }
+}
+class Bird extends Animal{
+    fly(){
+        return '파닥파닥';
+    }
+    isBird(){
+        return true;
+    }
+}
+class Penguin extends Bird{
+    override fly(){
+        throw new Error('못날아');  
+        //type:never return
+        //부모의 타입을 자식이 다르게 정의하면 위반됨
+    }
+}
+
+console.log(new Animal().isAnimal()); //true
+console.log(new Penguin().fly().at(1)); //error
+console.log(new Penguin().isAnimal()); //false
+
+
+//ISP
+interface IBird{    
+    fly(): string;
+    quack(): string;
+    //필요이상의 속성을 가져서 활용되지 못함
+    //분리할 필요가 있음음
+}
+class Bird extends Animal implements IBird{
+    fly(){
+        return '파닥파닥';
+    }
+    quack(){
+        return '짹짹';
+    }
+    isBird(){
+        return true;
+    }
+}
+class Penguin extends Bird{
+    fly(){
+        throw new Error('못날아')
+    }
+}
+
+//DIP
+interface Grimpan{}
+class Grimpan implements IGrimpan{
+    private static instance: Grimpan;
+    private constructor(canvas: HTMLElement | null){
+        if(!canvas || !(canvas instanceof HTMLCanvasElement)){
+            throw new Error('canvas엘리먼트 입력')
+        }
+    }
+}
+function main(grimpan: Grimpan){
+    grimpan.initialize();
+}
+
+main(Grimpan.getInstance())
+main(IEGrimpan.getInstance())
+main(ChromeGrimpan.getInstance())
+
+
+interface IObj{}
+class Obj implements IObj{}
+
+class A{
+    constructor(obj?: IObj){}
+    setObj(obj: IObj){}
+}
+new A(new Obj())
+new A().setObj(new Obj())
--- dist/GRIMPAN/src/grimpan.js
+++ dist/GRIMPAN/src/grimpan.js
@@ -18,3 +18,89 @@ class Grimpan {
     }
 }
 export default Grimpan;
+//OCP: 개방폐쇄 원칙
+//위반
+function main(type) {
+    if (type === 'a') {
+        doA();
+    }
+    else if (type === 'b') {
+        doB();
+    }
+    else if (type === 'c') {
+        doC();
+    }
+    else if (type === 'd') {
+        doD();
+    }
+    else {
+    }
+}
+function main(type) {
+    type.do();
+}
+const a = { do() { } };
+const b = { do() { } };
+const c = { do() { } };
+//LSP: 리스코프치환 원칙
+class Animal {
+    isAnimal() {
+        return true;
+    }
+}
+class Bird extends Animal {
+    fly() {
+        return '파닥파닥';
+    }
+    isBird() {
+        return true;
+    }
+}
+class Penguin extends Bird {
+    fly() {
+        throw new Error('못날아');
+        //type:never return
+        //부모의 타입을 자식이 다르게 정의하면 위반됨
+    }
+}
+console.log(new Animal().isAnimal()); //true
+console.log(new Penguin().fly().at(1)); //error
+console.log(new Penguin().isAnimal()); //false
+class Bird extends Animal {
+    fly() {
+        return '파닥파닥';
+    }
+    quack() {
+        return '짹짹';
+    }
+    isBird() {
+        return true;
+    }
+}
+class Penguin extends Bird {
+    fly() {
+        throw new Error('못날아');
+    }
+}
+class Grimpan {
+    static instance;
+    constructor(canvas) {
+        if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
+            throw new Error('canvas엘리먼트 입력');
+        }
+    }
+}
+function main(grimpan) {
+    grimpan.initialize();
+}
+main(Grimpan.getInstance());
+main(IEGrimpan.getInstance());
+main(ChromeGrimpan.getInstance());
+class Obj {
+}
+class A {
+    constructor(obj) { }
+    setObj(obj) { }
+}
+new A(new Obj());
+new A().setObj(new Obj());
